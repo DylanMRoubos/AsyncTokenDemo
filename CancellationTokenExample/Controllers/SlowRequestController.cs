805b015655bc105f03e8ac47515b34fd04e4388c
Dylan Roubos <dylanroubos@Dylans-MacBook-Pro.local>
2021-02-09 18:53:01 +0100

    Base base functionality for Async Token Demo

---
+++ CancellationTokenExample/Controllers/SlowRequestController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CancellationTokenExample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SlowRequestController : Controller
+    {
+        private readonly ILogger _logger;
+
+        public SlowRequestController(ILogger<SlowRequestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("/slowtest")]
+        public async Task<string> Get()
+        {
+            _logger.LogInformation("Starting to do slow work");
+
+            await Task.Delay(10_000);
+
+            var message = "Finished slow delay of 10 seconds.";
+
+            _logger.LogInformation(message);
+
+            return message;
+        }
+
+        [HttpGet("/slowtestwithcancel")]
+        public async Task<string> Get(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Starting to do slow work");
+
+            await Task.Delay(10_000, cancellationToken);
+
+            var message = "Finished slow delay of 10 seconds.";
+
+            _logger.LogInformation(message);
+
+            return message;
+        }
+    }
+}
